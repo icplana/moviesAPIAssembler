1cf12871121d88ac8403c365647e3126eb1b6375
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-05-23 13:21:15 +0200

    changing stat command

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import prisma from "./db/prismaClient";
 
 const PORT = process.env.PORT || 4000;;
 
-app.listen(PORT, "0.0.0.0", async () => {
+app.listen(PORT,  async () => {
 	console.log(`Server is running on port ${PORT}`);
 	await prisma.$connect();
 	console.log("Prisma connected");
