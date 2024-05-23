7c2c86eb7fd59e7fd1794a05a9752e704448ef0d
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-05-23 13:34:43 +0200

    adding cors config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from "express";
+
 import userRouter from "./routes/user.routes";
 import movieRoutes from "./routes/movie.routes";
 import genresRoutes from "./routes/genres.routes";
 
+const cors = require("cors")
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+  }));
+
+  
 app.use("/user", userRouter);
 app.use("/movie", movieRoutes);
 app.use("/genres", genresRoutes);
